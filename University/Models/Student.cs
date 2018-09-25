e23c58b7ffa89c9612b6465428b41a53b89de352
Mel Yasuda and Regina Nurieva <mel@>
2018-09-25 09:58:01 -0700

    add Save method

--- University/Models/Student.cs
+++ University/Models/Student.cs
@@ -5,8 +5,59 @@ using University;
 
 namespace University.Models
 {
-    public class CustomClassHere
-    {
+  public class Student
+  {
+    private int _id;
+    private string _name;
+    private Date _date;
 
+    public Student(int id, string name, Date date)
+    {
+      _id = id;
+      _name = name;
+      _date = date;
     }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public Date GetDate()
+    {
+      return _date;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (name, date) VALUES (@name, @date);";
+
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@name";
+      name.Value = this._name;
+      cmd.Parameters.Add(name);
+
+      MySqlParameter date = new MySqlParameter();
+      date.ParameterName = "@date";
+      date.Value = this._date;
+      cmd.Parameters.Add(date);
+
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+  }
 }
